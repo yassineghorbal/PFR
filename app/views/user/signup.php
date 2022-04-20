5f2c304277575f988bca372ae3eaa2821ea73499
yassine ghorbal <yassineghorbal2000@gmail.com>
2022-04-20 11:32:37 +0000

    login css changes

---
+++ app/views/user/signup.php
@@ -0,0 +1,45 @@
+<?php require APPROOT . "/views/inc/header.php"; ?>
+
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/login.css">
+</head>
+<body>
+
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MobiShop</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</div>
+
+<div class="container">
+    <form id="login_form">
+    <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    
+    <button type="submit" class="btn btn-primary w-100">Submit</button>
+    </form>
+</div>
+
+<?php require APPROOT . "/views/inc/footer.php"; ?>
