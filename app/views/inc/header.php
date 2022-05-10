88af81b46b1d5f00158210f209ec49937912af81
Yassine <yassineghorbal2000@gmail.com>
2022-05-10 10:50:26 +0100

    some modifications

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -22,12 +22,12 @@
 <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-white">
   <div class="container-fluid">
-    <a class="navbar-brand h1" href="http://localhost/MobiShop/products/">MobiShop</a>
+    <a class="navbar-brand h1" href="http://localhost/MobiShop/products/">Mobi<span class="color-danger">Shop</span></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-       <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 ">
         <li class="nav-item h6">
           <a class="nav-link nav_link" href="http://localhost/MobiShop/user/cart"><i class="bi bi-cart-fill"></i> Cart</a>
         </li>
